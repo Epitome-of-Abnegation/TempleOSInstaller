f34be465b2dbeecb3a0eea6e38e01a7ccad9a6cd
Steven Laczko <stevendlaczko@gmail.com>
2022-04-25 20:43:29 -0400

    extra if statement deleted

--- linux/mount.sh
+++ linux/mount.sh
@@ -4,14 +4,7 @@ set -xe
 
 . ./config.sh
 
-$NAME = "temple-img.iso"
-if [ $# -lt 1 ]; then
-    echo "Using $1"
-    $NAME=$1
-fi
-
-
 sync
 sudo umount $QEMU_IMG_MOUNT_DIR || true
 mkdir -p $QEMU_IMG_MOUNT_DIR
-sudo mount -o loop,offset=$QEMU_IMG_MOUNT_OFFSET,rw,uid=`id -u`,gid=`id -g` "$NAME" $QEMU_IMG_MOUNT_DIR
+sudo mount -o loop,offset=$QEMU_IMG_MOUNT_OFFSET,rw,uid=`id -u`,gid=`id -g` "$QEMU_IMG_NAME" $QEMU_IMG_MOUNT_DIR
