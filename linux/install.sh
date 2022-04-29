1bc853998fb221934481565a2de81e7856ada4df
Eccentrici <austineccentrici@gmail.com>
2022-04-29 13:44:20 +1000

    Added QEMU to each install

--- linux/install.sh
+++ linux/install.sh
@@ -4,17 +4,17 @@ if ! type "qemu-io" > /dev/null; then # Checking that QEMU exists
 
   # Package Manager specific; better to define manually than actually building from source (too long).
   declare -A osInfo;
-  osInfo[/etc/redhat-release]="yum install"
-  osInfo[/etc/arch-release]="pacman -S"
-  osInfo[/etc/gentoo-release]="emerge"
-  osInfo[/etc/SuSE-release]="zypper install"
-  osInfo[/etc/debian_version]="apt install -y"
-  osInfo[/etc/alpine-release]="apk add"
+  osInfo[/etc/redhat-release]="yum install qemu"
+  osInfo[/etc/arch-release]="pacman -S qemu"
+  osInfo[/etc/gentoo-release]="emerge qemu"
+  osInfo[/etc/SuSE-release]="zypper install qemu"
+  osInfo[/etc/debian_version]="apt install -y qemu qemu-utils qemu-system"
+  osInfo[/etc/alpine-release]="apk add qemu"
 
   for f in ${!osInfo[@]}
   do
       if [[ -f $f ]];then
-          sudo ${osInfo[$f]} qemu
+          sudo ${osInfo[$f]}
       fi
   done
 fi
